689092e063647ce709ae2a7ed6b97410353b57a8
paigeman <poizijiang1120@foxmail.com>
2021-12-05 21:05:24 +0800

    Groovy 映射

---
+++ BasicGrammar/src/main/groovy/GroovyMap.groovy
@@ -0,0 +1,16 @@
+/**
+* @author fade
+* @since 2021/12/05
+*/
+class GroovyMap {
+
+    static void main(String[] args) {
+        def a = [:]
+        def b = ['id': 1]
+        def c = [id: 1]
+        println a
+        println b
+        println c
+    }
+
+}
