69abd9eb88e2d3c5a417799a4759929149ccd738
paigeman <poizijiang1120@foxmail.com>
2021-12-05 21:56:34 +0800

    Groovy 异常处理

---
+++ BasicGrammar/src/main/groovy/GroovyRegex.groovy
@@ -0,0 +1,19 @@
+/**
+*   groovy正则表达式
+*
+* @author fade
+* @since 2021/12/05
+*/
+class GroovyRegex {
+
+    static void main(String[] args) {
+        def a = ~'Groovy'
+        def b = ('Hello Groovy'=~ '[\\s\\S]*Groovy')
+        def c = ('Hello Groovy' ==~ '[\\s\\S]*Groovy')
+        def d = ('Groovy' ==~ 'oo')
+        println b.matches()
+        println c
+        println d
+    }
+
+}
