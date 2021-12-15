db8c7a0db08a95de009eb8655fc2589277e38394
paigeman <poizijiang1120@foxmail.com>
2021-12-15 21:18:58 +0800

    Groovy closure
    Groovy closure

--- BasicGrammar/src/main/groovy/org/fade/demo/groovydemo/basicgrammar/GroovyClosure.groovy
+++ BasicGrammar/src/main/groovy/org/fade/demo/groovydemo/basicgrammar/GroovyClosure.groovy
@@ -11,6 +11,11 @@ class GroovyClosure {
     static void main(String[] args) {
         example()
         receiveParameter()
+        receiveImplicitParameter()
+        quoteVariable()
+        useInMethod()
+        closureWithList()
+        closureWithMap()
     }
 
     static void example() {
@@ -21,6 +26,42 @@ class GroovyClosure {
     static void receiveParameter() {
         def clos = {param->println "Hello ${param}"}
         clos.call("World")
+    }
+
+    static void receiveImplicitParameter() {
+        def clos = {println "Hello ${it}"}
+        clos.call("World")
+    }
+
+    static void quoteVariable() {
+        def str1 = "Hello"
+        def clos = {param -> println "${str1} ${param}"}
+        clos.call("World")
+
+        // We are now changing the value of the String str1 which is referenced in the closure
+        str1 = "Welcome"
+        clos.call("World")
+    }
+
+    static void display(Closure closure) {
+        closure.call("Inner")
+    }
+
+    static void useInMethod() {
+        def str1 = "Hello"
+        def clos = { param -> println "${str1} ${param}" }
+        display clos
+    }
+
+    static void closureWithList() {
+        def lst = [11, 12, 13, 14]
+        lst.each {println it}
+    }
+
+    static void closureWithMap() {
+        def mp = ["TopicName" : "Maps", "TopicDescription" : "Methods in Maps"]
+        mp.each {println it}
+        mp.each {println "${it.key} maps to: ${it.value}"}
     }
 
 }
