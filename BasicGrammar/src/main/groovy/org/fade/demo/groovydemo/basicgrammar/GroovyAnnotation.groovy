364f27910e996868eaa1bd866b47beb6c7b3e8a9
paigeman <poizijiang1120@foxmail.com>
2021-12-15 21:31:46 +0800

    Groovy annotation

---
+++ BasicGrammar/src/main/groovy/org/fade/demo/groovydemo/basicgrammar/GroovyAnnotation.groovy
@@ -0,0 +1,20 @@
+package org.fade.demo.groovydemo.basicgrammar
+
+/**
+*   groovy注解
+*
+* @author fade
+* @date 2021 /12/15
+*/
+class GroovyAnnotation {
+
+    static void main(String[] args) {
+
+    }
+
+}
+
+@interface Anywhere {
+
+}
+
