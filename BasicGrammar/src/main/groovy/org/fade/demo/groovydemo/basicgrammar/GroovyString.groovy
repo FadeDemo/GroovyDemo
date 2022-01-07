73c3158b1235a8b34f729408a207ca77a6661913
paigeman <53284808+paigeman@users.noreply.github.com>
2022-01-07 10:20:27 +0800

    groovy stripIndent() and stripMargin() method

--- BasicGrammar/src/main/groovy/org/fade/demo/groovydemo/basicgrammar/GroovyString.groovy
+++ BasicGrammar/src/main/groovy/org/fade/demo/groovydemo/basicgrammar/GroovyString.groovy
@@ -1,4 +1,7 @@
 package org.fade.demo.groovydemo.basicgrammar
+
+import org.codehaus.groovy.runtime.StringGroovyMethods
+
 /**
 *   groovy的字符串
 *
@@ -15,6 +18,16 @@ Groovy'''
         def d = """Hello \
 Groovy"""
         def e = "Hello Groovy\n" * 3
+        def f = '''\
+abc'''
+        def g = '''
+abc'''
+        def h = '''  a
+    b
+  c'''
+        def i = '''a
+    |b
+   |c'''
         println a
         println b
         println c
@@ -23,6 +36,17 @@ Groovy"""
         println a[-1]
         println a[0..-1]
         println e
+        assert !f.startsWith("\n")
+        assert g.startsWith("\n")
+        println f
+        println g
+        // stripIndent方法作用是去除String中每一行最前面的空格
+        // 去除空格的数量是由空格最少的行的最前面的空格数决定
+        // 对控制字符\t \n 等不起作用
+        println StringGroovyMethods.stripIndent(h)
+        // stripMargin方法作用是去除String中每一行最前面的空格和控制符（\n）除外
+        // 空格和控制符后面需跟着 |
+        println StringGroovyMethods.stripMargin(i)
     }
 
 }
