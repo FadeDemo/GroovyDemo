84f2fab2e8e720fb44e3f8f69ddb4706518de101
paigeman <poizijiang1120@foxmail.com>
2022-01-15 17:39:37 +0800

    Groovy array

---
+++ BasicGrammar/src/main/groovy/org/fade/demo/groovydemo/basicgrammar/GroovyArray.groovy
@@ -0,0 +1,18 @@
+package org.fade.demo.groovydemo.basicgrammar
+
+/**
+*   groovy数组
+*
+* @author fade
+* @date 2022 /01/15
+*/
+class GroovyArray {
+
+    static void main(String[] args) {
+        def a = [1, 2] as int[]
+        println a
+        int[] b = [1, 2]
+        println b
+    }
+
+}
