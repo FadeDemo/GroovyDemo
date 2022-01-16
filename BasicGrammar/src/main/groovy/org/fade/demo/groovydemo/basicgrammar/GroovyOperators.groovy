9d87c0d3b05ed89c4ca5ef55b703e82d23732b95
paigeman <poizijiang1120@foxmail.com>
2022-01-16 11:34:44 +0800

    Groovy object operators

--- BasicGrammar/src/main/groovy/org/fade/demo/groovydemo/basicgrammar/GroovyOperators.groovy
+++ BasicGrammar/src/main/groovy/org/fade/demo/groovydemo/basicgrammar/GroovyOperators.groovy
@@ -11,7 +11,11 @@ import groovy.transform.EqualsAndHashCode
 class GroovyOperators {
 
     static void main(String[] args) {
-        relationalOperators()
+//        relationalOperators()
+        elvisOperator()
+        safeNavigationOperator()
+        directFieldAccessOperator()
+        methodPointerOperator()
     }
 
     @EqualsAndHashCode
@@ -28,6 +32,54 @@ class GroovyOperators {
         assert cat.is(copyCat)  // Groovy identity
         assert cat === copyCat  // operator shorthand
         assert cat !== lion     // negated operator shorthand
+    }
+
+    static void elvisOperator() {
+        // elvis 运算符
+        def a = true
+        // a ?: false 等价于 a ? a : false
+        def b = a ?: false
+        println a
+        println b
+        // elvis 赋值运算符
+        def c
+        // c ?= false 应该等价于 c = c ? c : false
+        c ?= false
+        println c
+    }
+
+    static void safeNavigationOperator() {
+        // 安全引用操作符
+        def a = null
+        println a?.b
+    }
+
+    static class User {
+        public final String name
+        User(String name) { this.name = name}
+        String getName() { "Name: $name" }
+    }
+
+    static void directFieldAccessOperator() {
+        def user = new User('Bob')
+        assert user.@name == 'Bob'
+    }
+
+    static def doSomething(String str) { str.toUpperCase() }
+
+    static def doSomething(Integer x) { 2*x }
+
+    static void methodPointerOperator() {
+        // 方法指针运算符
+        def str = 'example of method reference'
+        // 方法指针可以看成是闭包
+        def fun = str.&toUpperCase
+        def upper = fun()
+        assert upper == str.toUpperCase()
+        // 当有多个匹配的方法时，方法指针会运行时匹配参数
+        def reference = this.&doSomething
+        assert reference('foo') == 'FOO'
+        assert reference(123)   == 246
     }
 
 }
