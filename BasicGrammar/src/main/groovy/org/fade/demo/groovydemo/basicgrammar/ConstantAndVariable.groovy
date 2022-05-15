8f722feebd199e941cdf14721e15f8b81e3c9077
paigeman <poizijiang1120@foxmail.com>
2022-05-15 16:41:00 +0800

    constant and variable

---
+++ BasicGrammar/src/main/groovy/org/fade/demo/groovydemo/basicgrammar/ConstantAndVariable.groovy
@@ -0,0 +1,27 @@
+package org.fade.demo.groovydemo.basicgrammar
+
+class ConstantAndVariable {
+
+    int a
+
+    private int b
+
+    public int c
+
+    static d
+
+    public static e
+
+    final f
+
+    public final g
+
+    static final h
+
+    public static final i
+
+    static void main(String[] args) {
+
+    }
+
+}
